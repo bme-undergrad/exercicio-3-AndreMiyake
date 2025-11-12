65d02d9c23f77115b3b075a2ae1d93f67171966a
André Nakano Miyake <andre.nmiyake@gmail.com>
2025-11-11 21:48:42 -0300

    Implement calculations for a1 and a0 in exercicio3.m
    Added calculations for a1 and a0 based on time and cell measurements.

--- exercicio3.m
+++ exercicio3.m
@@ -4,12 +4,24 @@ function [mu] = exercicio3(t, NC)
 % NC: medida do número de células a cada instante t
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
+time_quad = t.^2
 
-% digite seu codigo aqui
+N = length(t)
 
+sum_time = sum(t)
+sum_time_quad = sum(time_quad) 
+
+ln_cels = log(NC)
+sum_ln_cels = sum(ln_cels) 
+time_X_ln_cels = t.*ln_cels
+
+sum_do_produto = sum(time_X_ln_cels)
+
+a1 = (N*sum_do_produto - sum_time*sum_ln_cels)/(N*sum_time_quad - sum_time^2)
+a0 = sum_ln_cels/N - a1*(sum_time/N)
 
 % mantenha essas duas linhas finais, subtituindo mu pelo valor que você calculou
-mu = 0;
+mu = a1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
